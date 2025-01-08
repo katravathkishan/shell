1adc738cc8b0d7454bb2afef068e737d5ed89d8d
katravathkishan <creatergani@gmail.com>
2025-01-08 02:10:59 -0600

    file added

---
+++ 03-veriables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=ramesh #no space before or after equal
+person2=suresh
+
+echo "$person1:: Hi $person2"
+echo "$person2:: hello $person1"
+echo "$person1:: how are doing?"
+echo "$person2:: i am good. how are you?"
