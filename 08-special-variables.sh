bc9fbb6f2819d7c306bf62ddbc66fc40c7321e6a
katravathkishan <creatergani@gmail.com>
2025-01-08 09:16:37 -0600

    added datatypes

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -5,7 +5,7 @@ echo "number of veriables: $#"
 echo "script name: $0"
 echo "pwd: $pwd"
 echo "home directory of current user: $HOME"
-ech "which user is running this script: $user"
+ech "which user is running this script: $USER"
 echo "process id of current script: $$"
 sleep 60 &
 echo "process id of last command in background: $!"
