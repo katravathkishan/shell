c6af7b2d2e4a8f10b3ebea0561c6bb70fed86484
katravathkishan <creatergani@gmail.com>
2025-01-08 08:58:01 -0600

    added datatypes

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("PUSHP1" "PUSHP2" "RRR")#index start from 0
+
+echo "first movie: $MOVIES[0]"
+echo "SECOND movie: $MOVIES[1]"
+echo "third movie: $MOVIES[2]"
+echo "all movie: $MOVIES[@]"
