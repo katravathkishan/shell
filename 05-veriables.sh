2785e1ceb62532635596de493d6df5e63207c48b
katravathkishan <creatergani@gmail.com>
2025-01-08 02:30:06 -0600

    file added

---
+++ 05-veriables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo " enter the username::"
+
+read -s USERNAME
+
+echo "entered username: $USERNAME"
+
+echo "enter your password"
+
+read -s PASSWORD
+
+echo "entered password: $PASSWORED"
