43f3ef1fdc780248a9ec811c86ccf0da67735875
katravathkishan <creatergani@gmail.com>
2025-01-14 11:40:29 -0600

    install mysql added

---
+++ 13-colors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "installing MySQL ... $R FAILURE"
+        exit 1
+    else
+        echo -e "installing MySQL ... $G SUCCESS"
+    fi
+}
+
+if [ $UPER -ne 0 ]
+then 
+    echo "ERROR:: you must have sudo access to execute this script"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    VALIDATE $? "installing MySQL"
+else
+    echo -e "$Y MySQL is already installed"
+fi
+
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then 
+    dnf install git -y
+    VALIDATE $? "installing git"
+else
+    echo -e "$Y git is already installed"
+fi
