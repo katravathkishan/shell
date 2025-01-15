b9398eb38e51f2c81eaa7fabf5cfccca5a8eab30
katravathkishan <creatergani@gmail.com>
2025-01-15 14:09:09 -0600

    LOG FILES ADDED

--- 14-logs.sh
+++ 14-logs.sh
@@ -12,10 +12,10 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo -e "installing MySQL ... $R FAILURE"
+        echo -e "$2 ... $R FAILURE"
         exit 1
     else
-        echo -e "installing MySQL ... $G SUCCESS"
+        echo -e "$2 ... $G SUCCESS"
     fi
 }
 
